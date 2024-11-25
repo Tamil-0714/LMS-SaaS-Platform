b0b9dd8e6353ceecce57bc8408f5f42e4ba97a5d
Tamil-0714 <tom.jerry.07.05.06@gmail.com>
2024-11-25 11:08:37 +0530

    calendar componet added and tested

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import OAuth from "./components/OAuth";
 import { Button } from "./components/ui/button";
+import { Calendar } from "./components/ui/calendar";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    console.log(date);
+  }, [date]);
 
   return (
     <>
       <OAuth />
       <Button>Click me</Button>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border shadow"
+        classNames={{
+          day_selected:
+            "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+        }}
+      />
     </>
   );
 }
